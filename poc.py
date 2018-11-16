602f20b6631acf00a7436f5e5761f5a5d8a67105
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-11-16 17:40:45 +0100

    simplify poc

--- poc.py
+++ poc.py
@@ -1,14 +1,14 @@
 from PIL import Image
 import pytesseract
 
-IMAGE_TEMPLATE = 'data/eng.Fixedsys.exp{}.png'
-IMAGE = Image.open(IMAGE_TEMPLATE.format(1))
+IMAGE = 'data/eng.Fixedsys.exp2.png'
+IMAGE = 'data/base_image_3.png'
 
 # Simple image to string
 print(pytesseract.image_to_string(IMAGE, lang='Fixedsys'))
 
 # Get bounding box estimates
-print(pytesseract.image_to_boxes(IMAGE, lang='Fixedsys'))
+# print(pytesseract.image_to_boxes(IMAGE, lang='Fixedsys'))
 
 # Get verbose data including boxes, confidences, line and page numbers
 # print(pytesseract.image_to_data(IMAGE))
